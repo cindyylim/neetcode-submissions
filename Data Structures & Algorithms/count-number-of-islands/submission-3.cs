162c68ea0ad688cf51bd9e437257d2042b0255b5
Cindy L. <154645856+cindyylim@users.noreply.github.com>
2026-08-29 16:14:19 -0700

    Add: count-number-of-islands - submission-3

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-3.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int NumIslands(char[][] grid) {
+        var visited = new HashSet<(int row, int col)>();
+        int numIslands = 0;
+        for(int i =0; i < grid.Length; i++) {
+            for(int j = 0; j < grid[0].Length; j++) {
+                if (grid[i][j] == '1' && !visited.Contains((i, j))){
+                    Dfs(i, j, grid, visited);
+                    numIslands++;
+                }
+            }
+        }
+        return numIslands;
+    }
+    private void Dfs(int r, int c, char[][] grid, HashSet<(int row, int col)> visited) {
+        if (r < 0 || r >= grid.Length || c < 0 || c >= grid[0].Length || visited.Contains((r,c)) || grid[r][c] == '0') {
+            return;
+        }
+        visited.Add((r, c));
+        Dfs(r + 1, c, grid, visited);
+        Dfs(r - 1, c, grid, visited);
+        Dfs(r, c + 1, grid, visited);
+        Dfs(r, c - 1, grid, visited);
+    }
+}
