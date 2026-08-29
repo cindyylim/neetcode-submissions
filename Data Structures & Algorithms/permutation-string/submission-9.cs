d8f952bdb493e5c5e6e2750aad0e29c9d39cd25b
Cindy L. <154645856+cindyylim@users.noreply.github.com>
2026-08-29 13:28:18 -0700

    Add: permutation-string - submission-9

---
+++ Data Structures & Algorithms/permutation-string/submission-9.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public bool CheckInclusion(string s1, string s2) {
+        if (s1.Length > s2.Length) {
+            return false;
+        }
+        int start = 0;
+        int[] count = new int[26];
+        foreach (char c in s1) {
+            count[c-'a']++;
+        }
+        int[] window = new int[26];
+        for(int i =0; i < s2.Length; i++) {
+            window[s2[i]-'a']++;
+            if (i - start + 1 > s1.Length) {
+                window[s2[start]-'a']--;
+                start++;
+            }
+            if (window.SequenceEqual(count)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
