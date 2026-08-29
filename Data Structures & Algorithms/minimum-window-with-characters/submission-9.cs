09e69557d5fd34d02b2a17f7ef690a5f5e64f43a
Cindy L. <154645856+cindyylim@users.noreply.github.com>
2026-08-29 13:38:23 -0700

    Add: minimum-window-with-characters - submission-9

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-9.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public string MinWindow(string s, string t) {
+        Dictionary<char, int> freq = new Dictionary<char, int>();
+        foreach(char c in t) {
+            freq[c] = freq.GetValueOrDefault(c) + 1;
+        }
+        int needed = freq.Count;
+        int start = 0;
+        int minLength = int.MaxValue;
+        int minStart = 0;
+        for(int i = 0; i < s.Length; i++) {
+            if (freq.ContainsKey(s[i])) {
+                freq[s[i]] = freq[s[i]] - 1;
+                if (freq[s[i]] == 0) {
+                    needed--;
+                }
+            }
+            while (needed == 0 && start < s.Length) {
+                int windowLength = i - start + 1;
+                if (windowLength < minLength) {
+                    minLength = windowLength;
+                    minStart = start;
+                }
+                if (freq.ContainsKey(s[start])){
+                    freq[s[start]]++;
+                    if (freq[s[start]] > 0) {
+                        needed++;
+                    }
+                }
+                start++;
+            }
+        }
+        return minLength == int.MaxValue ? "" : s.Substring(minStart, minLength);
+    }
+}
