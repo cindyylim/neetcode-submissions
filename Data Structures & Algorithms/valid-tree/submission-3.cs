6ba1c52d8a4a26be3679f9e2b4de4871ac9bcbb0
Cindy L. <154645856+cindyylim@users.noreply.github.com>
2026-08-29 22:01:29 -0700

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public bool ValidTree(int n, int[][] edges) {
+        if (edges.Length != n -1) {
+            return false;
+        }
+        List<List<int>> graph = new List<List<int>>();
+        for (int i = 0; i < n; i++) {
+            graph.Add(new List<int>());
+        }
+        foreach(int[] edge in edges) {
+            int from = edge[0];
+            int to = edge[1];
+            graph[from].Add(to);
+            graph[to].Add(from);
+        }
+        Queue<int> queue = new Queue<int>();
+        queue.Enqueue(0);
+        HashSet<int> visited = new HashSet<int>();
+        while (queue.Count > 0) {
+            int node = queue.Dequeue();
+            if (visited.Contains(node)){
+                continue;
+            }
+            visited.Add(node);
+            List<int> neighbors = graph[node];
+            foreach(int neighbor in neighbors) {
+                queue.Enqueue(neighbor);
+            }
+        }
+        return visited.Count == n;
+    }
+}
